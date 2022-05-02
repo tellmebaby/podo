a01d5115e74657f6d0b55dda9a6194682a675b50
tellmebaby <tellmebaby407@gmail.com>
2022-05-02 22:45:26 +0900

    Initial Commit

---
+++ podo/podoApp.swift
@@ -0,0 +1,17 @@
+//
+//  podoApp.swift
+//  podo
+//
+//  Created by Sung Hong on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct podoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
